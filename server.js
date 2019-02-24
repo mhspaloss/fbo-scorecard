065b67c533be8b929c1b661f3322f540d42250ed
Glitch (fbo-scorecard) <none>
2019-02-24 01:05:57 +0000

    🌿🦃Added code to update records as interesting opps or not

--- server.js
+++ server.js
@@ -91,47 +91,16 @@ app.get('/opps/edit/:id', function (req, res) {
   }); 
 });
 
-//Process Opp Form
-app.post('/opps', function (req, res) {
-  let errors = [];
-  
-  if(!req.body.title) {
-  errors.push({text: 'Please add a title'});
-  }
-  if(!req.body.details) {
-  errors.push({text: 'Please add some details'});
-  }
-  
-  if(errors.length > 0){
-    res.render('ideas/add', {
-      errors: errors,
-      title: req.body.title,
-      details: req.body.details
-    });
-  } else {
-    const newUser = {
-    title: req.body.title,
-    details: req.body.details
-    }
-    new Idea(newUser)
-    .save()
-    .then(idea => {
-      res.redirect('opps');
-    })
-  }
-});
-
 //Edit Opp Form Process
 app.put('/opps/:id', function (req, res) {
-  Idea.findOne({
+  Presol.findOne({
     _id: req.params.id
   })
-  .then(idea => {
+  .then(presol => {
   //New values
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-    idea.save()
-      .then(idea => {
+    presol.isInteresting = req.body.isInteresting;
+    presol.save()
+      .then(opps => {
         res.redirect('/opps');
     })
   });
@@ -165,7 +134,7 @@ app.get('/ideas/edit/:id', function (req, res) {
   }); 
 });
 
-//Process Form
+//Process Idea Form
 app.post('/ideas', function (req, res) {
   let errors = [];
   
